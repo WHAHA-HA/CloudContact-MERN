1a50a91b2872c67a5e495cbaed174c6ad44e3d5a
StenEvers <sten1991001@outlook.com>
2019-11-16 00:44:57 +0600

    Redux Devtools Removed

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,7 @@ import thunk from "redux-thunk";
 import rootReducer from "./store/reducers/rootReducer";
 import App from "./App";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
